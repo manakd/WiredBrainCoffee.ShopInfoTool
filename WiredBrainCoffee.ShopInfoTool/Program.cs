00cefe0ce9ee4e81e4239aa844590792252a75b7
Doug Manak <dougmanak@gmail.com>
2026-01-09 23:08:32 -0600

    Implement quit command

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -1,5 +1,17 @@
-﻿Console.WriteLine("-------------------------------------"); 
+﻿using System.Security.Cryptography.X509Certificates;
+
+Console.WriteLine("-------------------------------------"); 
 Console.WriteLine(" Wired Brain Coffee - Shop Info Tool ");
 Console.WriteLine("-------------------------------------");
+Console.WriteLine();
+Console.WriteLine("Commands:");
+Console.WriteLine("  quit   exit application");
 
-Console.ReadLine();
+while (true)
+{
+    var command = Console.ReadLine();
+    if (string.Equals("quit",command,StringComparison.OrdinalIgnoreCase))
+    {
+        break; 
+    }
+}
