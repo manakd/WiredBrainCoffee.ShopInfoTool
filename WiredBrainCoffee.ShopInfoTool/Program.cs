8ab065368f2fe11847e512406ec5f59ab9ffe405
Doug Manak <dougmanak@gmail.com>
2026-01-10 20:05:49 -0600

    Implement shops command

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System.Security.Cryptography.X509Certificates;
+using WiredBrainCoffee.ShopInfoTool.Data;
 
 Console.WriteLine("-------------------------------------"); 
 Console.WriteLine(" Wired Brain Coffee - Shop Info Tool ");
 Console.WriteLine("-------------------------------------");
 Console.WriteLine();
 Console.WriteLine("Commands:");
+Console.WriteLine("  shops  show coffeeshops");
 Console.WriteLine("  quit   exit application");
 
 while (true)
@@ -13,5 +15,15 @@ while (true)
     if (string.Equals("quit",command,StringComparison.OrdinalIgnoreCase))
     {
         break; 
+    }
+    else if (string.Equals("shops", command, StringComparison.OrdinalIgnoreCase))
+    {
+        var dataProvider = new CoffeeShopDataProvider();
+        var coffeeShops = dataProvider.LoadShops();
+
+        foreach (var coffeeShop in coffeeShops)
+        {
+            Console.WriteLine(coffeeShop);
+        }
     }
 }
